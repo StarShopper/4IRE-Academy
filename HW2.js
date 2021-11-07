23272785dbb9a3781189b24fa1c237b50169f887
StarShopper <georg.stein10@gmail.com>
2021-11-07 20:04:39 +0200

    Final homeWork 2

--- HW2.js
+++ HW2.js
@@ -1,7 +1,26 @@
 alert("Fibonacci Sequence");
 
+function FibonSeqPos(firstNum, secNum, n) {
+    if (n === 1) {
+        return firstNum;
+    } else if (n === 2) {
+        return secNum;
+    } else {
+        let F1 = firstNum;
+        let F2 = secNum;
+        let F3;
+
+        for (let i = 2; i < n; i++) {
+            F3 = F1 + F2;
+            F1 = F2;
+            F2 = F3
+        }
+        return F3
+    }
+}
+
 function FibonSeqNeg(firstNum, secNum, n) {
-    if (n == 0) {
+    if (n === 0) {
         return alert(`Okay, let's try again`)
     } else {
         let F1 = firstNum;
@@ -17,38 +36,18 @@ function FibonSeqNeg(firstNum, secNum, n) {
     }
 }
 
-
-function FibonSeqPos(firstNum, secNum, n) {
-    if (n === 1) {
-        return firstNum;
-    } else if (n === 2) {
-        return secNum;
-    } else {
-        let F1 = firstNum;
-        let F2 = secNum;
-        let F3;
-
-        for (let i = 0; i < n; i++) {
-            F3 = F1 + F2;
-            F1 = F2;
-            F2 = F3
-        }
-        return F3
-    }
-}
-
 let firstNum = +prompt('Enter first num from Fibonacci seq.: (F1)', "");
-while (firstNum || isNaN(firstNum) || firstNum == "") {
+while ((firstNum.toFixed() != firstNum) || isNaN(firstNum || firstNum == "")) {
     firstNum = +prompt("try again", "");
 }
 
 let secNum = +prompt('Enter second num from Fibonacci seq.: (F2)', "");
-while (secNum || isNaN(secNum) || secNum == "") {
+while ((secNum.toFixed() != secNum) || isNaN(secNum || secNum == "")) {
     secNum = +prompt("try again", "");
 }
 
 let n = +prompt('Enter num from Fibonacci seq.: (n)', "");
-while (n || isNaN(n) || n == "") {
+while ((n.toFixed() != n)|| isNaN(n || n == "")) {
     n = +prompt("try again", "");
 }
 
