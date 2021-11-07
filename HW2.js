6515ece010bc6b023f7894daa9b5aac7b5909509
StarShopper <georg.stein10@gmail.com>
2021-11-07 18:05:30 +0200

    branching, huh

---
+++ HW2.js
@@ -0,0 +1,7 @@
+alert("Fibonacci Sequence");
+
+function FibonSeq(F1, F2, n){
+    if(n == 0 ){
+        return alert(`Okay, let's try again`)
+    }
+}
--- HW3.js
+++
@@ -1,1 +0,0 @@
-aletrt("Fibonacci Sequence");
